7a41f4cf314a45385b945bb88f410f2a998ae0eb
Ambjörn Fagerström <ambjorn.fagerstrom@fast2.se>
2022-11-10 16:53:58 +0100

    Navbar remove import

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,4 @@
-﻿import React, { ReactComponentElement } from 'react'
-import Link from 'next/link'
+﻿import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 
