fd9dcd4e3f9ae47544e5d550402f9c97f2ff3935
Ambjörn Fagerström <ambjorn89@gmail.com>
2022-12-05 17:07:18 +0100

    Add heroicons

--- components/Art.tsx
+++ components/Art.tsx
@@ -1,8 +1,10 @@
-﻿import React, { useState } from 'react'
+﻿import React, { useState, KeyboardEvent } from 'react'
 import { urlFor } from "../santity";
 import { Post } from "../typings";
 import { motion } from "framer-motion";
 import Image from 'next/image'
+import { ArrowSmallRightIcon, ArrowSmallLeftIcon, XMarkIcon } from '@heroicons/react/24/solid'
+
 
 type Props = {
   posts: Post[];
@@ -33,8 +35,6 @@ function Art({ posts, category, className }: Props) {
       <Image
         onClick={() => showImage(urlFor(mainImage).url(), (title + (year === undefined ? "" : " - " + year)))}
         src={urlFor(mainImage).url()}
-        placeholder='blur'
-        blurDataURL={urlFor(mainImage).url()}
         className="mb-4 cursor-pointer"
         alt={title}
         priority
@@ -45,24 +45,6 @@ function Art({ posts, category, className }: Props) {
       <p className="text-center">{title}{year && " - " + year}</p>
     </motion.div>
   ));
-
-  const exit = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-    </svg>
-  )
-
-  const arrowRight = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-    </svg>
-  )
-
-  const arrowLeft = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
-    </svg>
-  )
   
   //function to show a specific image in the lightbox, amd make lightbox visible
   const showImage = (image: any, text: string) => {
@@ -118,20 +100,39 @@ function Art({ posts, category, className }: Props) {
       {imageCards}
       {
         lightboxDisplay ? 
-        <div className="flex justify-between items-center overflow-y-hidden fixed inset-0 bg-black/90 p-4" onClick={hideLightBox}>
-          <button className="absolute right-4 top-4 z-10" onClick={hideLightBox}>{exit}</button>
-          <button className="bg-black text-white p-4 text-xl absolute left-0 z-10" onClick={showPrev}>{arrowLeft}</button>
+        <motion.div 
+          className="flex justify-between items-center overflow-y-hidden fixed inset-0 bg-black/90 p-4"
+          onClick={hideLightBox}
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+          }}
+        >
+          <button className="absolute right-4 top-4 z-10" onClick={hideLightBox}>
+            <XMarkIcon className="h-6 w-6 text-white"/>
+          </button>
+          <button className="bg-black text-white p-4 text-xl absolute left-0 z-10" onClick={showPrev}>
+            <ArrowSmallLeftIcon className="h-6 w-6 text-white"/>
+          </button>
           <Image 
             alt="image" 
-            className="p-14" 
+            className="p-14"
             fill
+            priority
             style={{
               objectFit: 'contain',
             }}
-            src={urlFor(imageToShow).url()} />
-          <p className="absolute right-0 left-0 text-center bottom-0 p-4 text-white z-10">{textToShow}</p>
-          <button className="bg-black text-white p-4 text-xl absolute right-0 z-10" onClick={showNext}>{arrowRight}</button>
-        </div>
+            src={urlFor(imageToShow).url()}
+          />
+          <p className="absolute right-0 left-0 text-center bottom-0 p-4 text-white z-10">
+            {textToShow}
+          </p>
+          <button className="bg-black text-white p-4 text-xl absolute right-0 z-10" onClick={showNext}>
+            <ArrowSmallRightIcon className="h-6 w-6 text-white"/>
+          </button>
+        </motion.div>
        : ""
       }
     </div>
